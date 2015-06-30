5c3bfae396e525156df4f1044bc50d89af3d34b3
kukujiabo <kukujiabo@163.com>
2015-06-30 16:33:04 +0800

    bucket add delete.

--- app/Http/Controllers/Bucket/BucketController.php
+++ app/Http/Controllers/Bucket/BucketController.php
@@ -3,6 +3,7 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Bucket;
+use App\Models\Goods;
 use Illuminate\Http\Request;
 
 class BucketController extends Controller {
@@ -90,9 +91,104 @@ class BucketController extends Controller {
   
     $uid = $request->input('uid');
   
-    $myBucket = Bucket::where('user_id', '=', $uid)->first();
+    $myBucket = Bucket::where('user_id', '=', $uid)
+        
+        ->where('active', '=', '1')
+          
+        ->get();
   
     return $this->successResponse('data', $myBucket);
+  
+  }
+
+  public function deleteBucket (Request $request) {
+  
+    $uid = $request->input('');
+
+    $code = $request->input('gcode');
+
+    $gid = $request->input('gid');
+
+    $bucket = null;
+  
+    if (empty($code)) {
+    
+      $bucket = Bucket::where('good_id', '=', $gid)->first();
+    
+    } else {
+    
+      $bucket = Bucket::where('ext_1', '=', $code)->first();
+    
+    }
+
+    $bucket->active = 0;
+
+    $bucket->save();
+
+    $this->successResponse();
+  
+  }
+
+  public function addBucket (Request $request) {
+  
+    $uid = $request->input('uid');
+
+    $code = $request->input('code');
+
+    if (empty($code)) {
+    
+      return $this->failResponse('code is required.');
+    
+    }
+
+    $gid = $request->input('gid');
+
+
+    if (empty($gid)) {
+    
+      return $this->failResponse('good id is required.');
+    
+    }
+
+    $good = Goods::find($gid);
+
+    if (empty($good)) {
+    
+      return $this->failResponse('good not found.');
+    
+    }
+
+    if ($good->code != $code) {
+    
+      return $this->failResponse('good code not match.');
+    
+    }
+
+    $ext_2 = $request->input('ext_2'); 
+
+    $nickname = $request->input('nick');
+
+    $comment = $request->input('comment');
+
+    $bucket = new Bucket;
+
+    $bucket->user_id = $uid;
+
+    $bucket->good_id = $gid;
+
+    $bucket->nick_name = empty($nickname) ? '' : $nickname;
+
+    $bucket->active = 1;
+
+    $bucket->comment = empty($comment) ? '' : $comment;
+
+    $bucket->ext_1 = $code;
+
+    $bucket->ext_2 = empty($ext_2) ? '' : $ext_2;
+
+    $bucket->save();
+
+    return $this->successResponse();
   
   }
 
