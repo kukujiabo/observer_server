9a43623c2c88225309acad7795127149d62ba94b
kukujiabo <kukujiabo@163.com>
2015-07-30 14:08:32 +0800

    asdasd

--- app/Http/Controllers/Customer/CustomersController.php
+++ app/Http/Controllers/Customer/CustomersController.php
@@ -3,7 +3,7 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
-use App\Models\CustomerExtrInfo;
+use App\Models\CustomerExtraInfo;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller {
